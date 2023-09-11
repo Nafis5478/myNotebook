ede2f35c94558ac652723675ef9105118b3484a2
Mohammad Nafis Raza <mohammad.nafis.raza.28@gmail.com>
2023-09-12 00:30:33 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Navbar } from "./Components/Navbar";
+import { About } from "./Components/About";
+import { Homi } from "./Components/Homi";
+import { NoteState } from "./context/notes/NoteState";
+import Alert from "./Components/Alert";
+import Loginn from "./Components/Loginn";
+import Signupp from "./Components/Signupp";
+import { useState } from "react";
+import Lander from "./Components/Lander";
 function App() {
+  const [alert, setAlert] = useState(null);
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NoteState>
+        <Router>
+          <Navbar showAlert={showAlert}/>
+          <Alert alert={alert} />
+
+          <Routes>
+            <Route exact path="/" element={<Lander showAlert={showAlert} />} />
+            <Route
+              exact
+              path="/notes"
+              element={<Homi showAlert={showAlert} />}
+            />
+            <Route exact path="/about" element={<About />} />
+            <Route
+              exact
+              path="/login"
+              element={<Loginn showAlert={showAlert} />}
+            />
+            <Route
+              exact
+              path="/signup"
+              element={<Signupp showAlert={showAlert} />}
+            ></Route>
+          </Routes>
+        </Router>
+      </NoteState>
+    </>
   );
 }
 
